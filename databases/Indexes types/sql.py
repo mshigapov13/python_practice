db4dacc9a8e120c04717743fcb5a51e8e91461d1
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-06-17 18:52:21 +0300

    Added indexes.

---
+++ databases/Indexes types/sql.py
@@ -0,0 +1,55 @@
+import mysql.connector
+import time
+from DBcm import UseDatabase
+from dbconfig import dbconfig
+import sql_commands
+from datetime import date
+from random import randint
+
+table_names = ['common_people', 'btree_people', 'hash_people']
+
+with UseDatabase(dbconfig) as cursor:
+    for table in table_names:
+        _SQL = sql_commands.BTree_table_create % (dbconfig['database'], table)
+        cursor.execute(_SQL)
+
+    start_dt = date.today().replace(year=2002).toordinal()
+    end_dt = date.today().replace(year=2018).toordinal()
+    gender = ['m', 'f']
+    for i in range(0, 10001):
+        if i % 10 == 0:
+            l_name = (chr(randint(ord('a'), ord('z'))) * 10) * 2
+            f_name = l_name
+        else:
+            l_name = chr(randint(ord('a'), ord('z'))) * 10
+            f_name = chr(randint(ord('a'), ord('z'))) * 10
+        dob = str(date.fromordinal(randint(start_dt, end_dt)))
+        rand_gender = gender[randint(0, 1)]
+        for table in table_names:
+            _SQL = sql_commands.table_insert % (dbconfig['database'], table, i, l_name, f_name, dob, rand_gender)
+            cursor.execute(_SQL)
+
+import common_table
+import btree
+import hash_table
+
+#common_table.common_table()
+start_time = time.time()
+common_table.select()
+print('Non-index executed: --- %s ---' % (time.time() - start_time))
+
+#btree.btree()
+start_time = time.time()
+btree.select()
+print('btree-index executed: --- %s ---' % (time.time() - start_time))
+
+#hash_table.hash_table()
+start_time = time.time()
+hash_table.select()
+print('hash-index executed: --- %s ---' % (time.time() - start_time))
+
+
+with UseDatabase(dbconfig) as cursor:
+    for table in table_names:
+        _SQL = sql_commands.table_drop % (dbconfig['database'], table)
+        cursor.execute(_SQL)
---
+++ databases/Indexes types/sql_commands.py
@@ -0,0 +1,36 @@
+Common_table_create = '''
+create table if not exists %s.%s(
+id int,
+last_name varchar(50),
+first_name varchar(50),
+dob date,
+gender enum('m','f')
+)
+'''
+
+BTree_table_create = '''
+create table if not exists %s.%s(
+id int,
+last_name varchar(50),
+first_name varchar(50),
+dob date,
+gender enum('m','f'),
+key (last_name, first_name, dob)
+)'''
+
+Hash_table_create = '''
+create table if not exists %s.%s(
+id int,
+last_name varchar(50),
+first_name varchar(50),
+dob date,
+gender enum('m','f'),
+key using hash(l_name)
+)'''
+
+
+table_insert = '''insert into %s.%s values (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\')'''
+
+table_drop = '''drop table %s.%s'''
+
+select = 'select count(last_name) from %s.%s where last_name = first_name'
