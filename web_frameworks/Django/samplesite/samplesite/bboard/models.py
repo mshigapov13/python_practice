307ec30d76802833c59e42546658ce4b33fa56e7
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-06-27 21:01:33 +0300

    Added samplesite project.

---
+++ web_frameworks/Django/samplesite/samplesite/bboard/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.DataTimeField(auto_now_add=True, db_index=True)
