314bdb4ca69f51d4b424bc762f1839bfa8845182
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-06-28 18:24:47 +0300

    Implemented ad list.

--- web_frameworks/Django/samplesite/bboard/views.py
+++ web_frameworks/Django/samplesite/bboard/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Bb
+
 def index(request):
-    return HttpResponse('Here will be displayed ad board')
+    s = 'Ad list\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
