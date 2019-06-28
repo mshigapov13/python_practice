a5cf0dc2bd711747dc70be08fae0155f57046f49
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-06-28 21:22:26 +0300

    Implemented html template.

--- web_frameworks/Django/samplesite/bboard/views.py
+++ web_frameworks/Django/samplesite/bboard/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Bb
 
 def index(request):
-    s = 'Ad list\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = [Bb.objects.all()
+    context = {'bbs':bbs}
+    return HttpResponse(template.render(context, request))
