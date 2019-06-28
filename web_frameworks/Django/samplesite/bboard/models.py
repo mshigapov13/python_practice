86f95e695771986a9b2943a8e7934f2227f542fc
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-06-28 17:58:26 +0300

    Added field.

--- web_frameworks/Django/samplesite/bboard/models.py
+++ web_frameworks/Django/samplesite/bboard/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Bb(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(null=True, blank=True)
-    price = models.DataTimeField(auto_now_add=True, db_index=True)
+    price = models.FloatField(null=True, blank=True)
+    published = models.DataTimeField(auto_now_add=True, db_index=True)
