71dff22a2794078fc8a202c3dd080e19da794c71
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-01 21:13:33 +0300

    Added settings to images processing.

--- web_frameworks/Django/adboard/bboard/bboard/settings.py
+++ web_frameworks/Django/adboard/bboard/bboard/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'bootstrap4'
+    'bootstrap4',
+    'django_cleanup',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+THUMBNAIL_ALIASES = {
+    '':{
+        'default':{
+            'size':(96,96),
+            'crop':'scale',
+        }
+    }
+}
+
+THUMBNAIL_BASEDIR = 'thumbnails'
--- web_frameworks/Django/adboard/bboard/bboard/urls.py
+++ web_frameworks/Django/adboard/bboard/bboard/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.contrib.staticfiles.views import serve
 from django.views.decorators.cache import never_cache
 
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='')),
@@ -26,3 +28,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
