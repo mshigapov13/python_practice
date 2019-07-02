f6e4239de6f28cb20daebcd6b890140e48d9c939
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-02 13:05:02 +0300

    Implemented rubric models.

--- web_frameworks/Django/adboard/bboard/main/models.py
+++ web_frameworks/Django/adboard/bboard/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, unique=True, verbose_name='Name')
+    order = models.SmallIntegerField(default=0, db_index=True, verbose_name='Order')
+    super_rubric = models.ForeignKey('SuperRubric', on_delete=models.PROTECT, null=True, blank=True, verbose_name='SuperRubric')
+
+class SuperRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric_isnull=True)
+
+class SuperRubric(Rubric):
+    objects = SuperRubricManager()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        proxy = True
+        ordering = ('order', 'name')
+        verbose_name = 'SuperRubric'
+        verbose_name_plural = 'SuperRubrics'
+
+class SubRubricManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(super_rubric_isnull=False)
+
+class SubRubric(Rubric):
+    objects = SubRubricManager()
+
+    def __str__(self):
+        return '%s - %s' % (self.super_rubric.name, self.name)
+    
+    class Meta:
+        proxy = True
+        ordering = ('super_rubric__order', 'super_rubric__name', 'order', 'name')
+        verbose_name = 'SubRubric'
+        verbose_name_plural = 'SubRubrics'
