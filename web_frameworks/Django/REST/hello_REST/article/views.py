b56b56ab198429d1402783aa72225b2c4772324f
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-08 19:19:59 +0300

    Implemented serializer.

--- web_frameworks/Django/REST/hello_REST/article/views.py
+++ web_frameworks/Django/REST/hello_REST/article/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Article
+from .serializers import ArticleSerializer
 
 
 class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
-        return Response({"articles": articles})
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
