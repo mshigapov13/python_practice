34a20940dc229df0cc47256f2dcb5cbffb16ca1e
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-08 19:59:17 +0300

    Implemented editing.

--- web_frameworks/Django/REST/hello_REST/article/views.py
+++ web_frameworks/Django/REST/hello_REST/article/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -18,3 +19,15 @@ class ArticleView(APIView):
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+    
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        
+        return Response({
+            "success": "Article '{}' updated successfully".format(article_saved.title)
+        })
