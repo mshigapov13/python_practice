2978a3fb7c2cff803de0033f27d357f7ff269e29
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-18 20:01:53 +0300

    Added test.

--- final_project/bboard/tests.py
+++ final_project/bboard/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Advertisement
 
@@ -10,3 +11,20 @@ class AdvertisementModelTest(TestCase):
         advertisement = Advertisement.objects.get(id=1)
         expected_obj_name = f'{advertisement.text}'
         self.assertEqual(expected_obj_name, 'just a test')
+
+class AdvertisementsPageViewTest(TestCase):
+    def setUp(self):
+        Advertisement.objects.create(text='this is another test')
+    
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('advs'))
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('advs'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'advs.html')
