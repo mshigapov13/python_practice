571d4a7749c5a773b019fff1f95a68c46cb5ecb2
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-18 18:47:07 +0300

    Added advertising board.

--- final_project/bboard/views.py
+++ final_project/bboard/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Advertisement
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Advertisement
+    template_name = 'advs.html'
+    context_object_name = 'all_advertisement_list'
+    
--- final_project/mysite/urls.py
+++ final_project/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('advs/', include('bboard.urls')),
     path('', include('pages.urls')),
 ]
