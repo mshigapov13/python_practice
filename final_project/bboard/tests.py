7ac666a61b785782ab5dfd0a3b01487a5c02307d
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-22 18:03:01 +0300

    Added test.

--- final_project/bboard/tests.py
+++ final_project/bboard/tests.py
@@ -19,6 +19,10 @@ class AdvertisementTests(TestCase):
             author=self.user,
         )
     
+    def test_string_representation(self):
+        advertisement = Advertisement(title='Sample title')
+        self.assertEqual(str(advertisement), advertisement.title)
+
     def test_adv_detail_view(self):
         response = self.client.get('/advs/adv/1/')
         no_response = self.client.get('/advs/adv/100000/')
@@ -33,9 +37,7 @@ class AdvertisementTests(TestCase):
         self.assertContains(response, 'Test body')
         self.assertTemplateUsed(response, 'advs.html')
 
-    def test_string_representation(self):
-        advertisement = Advertisement(title='Sample title')
-        self.assertEqual(str(advertisement), advertisement.title)
+    
 
     def test_adv_content(self):
         self.assertEqual(f'{self.advertisement.title}', 'Test title')
@@ -51,3 +53,14 @@ class AdvertisementTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'New Test title')
         self.assertContains(response, 'New Test text')
+
+    def test_adv_update_view(self):
+        response = self.client.post(reverse('adv_edit', args='1'),
+        {'title': 'Updated title', 'body': 'Updated body'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_adv_delete_view(self):
+        response = self.client.get(
+            reverse('adv_delete', args='1')
+        )
+        self.assertEqual(response.status_code, 200)
