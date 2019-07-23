85e09286b82822f312e541e76db7f1310a71ed26
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-23 20:35:06 +0300

    Changed project settings after removing unnecessery files and changing location html templates.

--- final_project/mysite/urls.py
+++ final_project/mysite/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('advs/', include('bboard.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', include('pages.urls')),
+    path('', include('bboard.urls')),
 ]
