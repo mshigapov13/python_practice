58f458d4253c482a2055811f45de0a712a89884c
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-26 19:07:16 +0300

    Migrated to Postgre.

--- final_project/mysite/settings.py
+++ final_project/mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_mysite_db',
+        'USER': 'django',
+        'PASSWORD': 'p@ssw0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
