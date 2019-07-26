5f7680684fe64438b7549907e032e73acc7c3dd5
mshigapov13@gmail.com <mshigapov13@gmail.com>
2019-07-26 20:19:02 +0300

    Substituted a custom User mode.

--- final_project/accounts/forms.py
+++ final_project/accounts/forms.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
+    class Meta:
+        model = get_user_model()
         fields = UserCreationForm.Meta.fields + ('age',)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = UserChangeForm.Meta.fields
